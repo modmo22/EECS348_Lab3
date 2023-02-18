a0962623f056df50be6195da97b8541804bed947
modco <fullmage72@ku.edu>
2023-02-17 18:39:45 -0600

    Adding input.txt and Program1.c file

--- Cprogram1/Program1.c
+++ Cprogram1/Program1.c
@@ -4,8 +4,33 @@
 
 #include <stdio.h>
 
-int main ()
-{
 
+int main () {
+    // create an array of months
+    char months[] = {"January", "February", "March", "April", "May", "June", "July",
+                      "August", "September", "October", "November", "December"};
+
+    // input a .txt file that has a floating point number on each line and put it into an array
+    FILE *file = fopen("input.txt", "r");
+//    if (file == NULL)
+//    {
+//        perror("Error opening file");
+//        return 1;
+//    }
+
+    float sales[12];
+    char line[256];
+    int i = 0;
+
+//    file = fopen("input.txt", "r");
+    while (fgets(line, sizeof(line), file))
+    {
+        sscanf(line, "%f", &sales[i]);
+        i++;
+    }
+    fclose(file);
+    for (i = 0; i < 12; i++)
+    {
+        printf("%s: %.2f", months[i], sales[i]);
+    }
 }
-
